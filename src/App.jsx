b6a2825f580fba70c52efce59c2f6dcf0ae2a5d7
maxdeng007 <your-email@example.com>
2026-03-10 16:34:51 +0800

    feat: clean responsive grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -26,18 +26,23 @@ function App() {
       <Hero />
       
       <main className="max-w-md mx-auto px-4 pb-8 md:max-w-2xl md:px-6 lg:max-w-6xl lg:px-8">
-        <div className="space-y-6 lg:flex lg:gap-8">
-          <div className="flex-1 lg:w-2/3 lg:order-1 order-1">
-            <HotNews />
+        <div className="space-y-6 lg:space-y-8">
+          <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+            <div className="lg:col-span-2">
+              <HotNews />
+            </div>
+            <div className="lg:col-span-1 lg:sticky lg:top-8">
+              <HoldingsImpact />
+            </div>
           </div>
           
-          <div className="lg:w-1/3 lg:sticky lg:top-8 lg:self-start space-y-6 lg:order-2 order-4">
-            <HoldingsImpact />
-            <Recommendations />
-          </div>
-          
-          <div className="lg:w-2/3 lg:order-3 order-2">
-            <Feedback />
+          <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+            <div className="lg:col-span-1">
+              <Recommendations />
+            </div>
+            <div className="lg:col-span-2">
+              <Feedback />
+            </div>
           </div>
         </div>
       </main>
